db3750646169b7c652ed70c8c51707816e4ef335
Karli Thomas <karliholthouse@gmail.com>
2022-03-08 16:25:15 -0800

    committing newrelic

--- utils/loadtest/100rps.js
+++ utils/loadtest/100rps.js
@@ -16,3 +16,26 @@ export default function() {
   const res = http.get(url);
   sleep(1);
 }
+
+
+// import http from 'k6/http';
+// import { sleep } from 'k6';
+
+// export const options = {
+//   vus: 1000,
+//   duration: '15s',
+// };
+
+
+// const id = Math.floor(Math.random() * 1000011);
+// const urlProduct = `http://localhost:3000/api/products/${id}`
+// const urlStyles = `http://localhost:3000/api/products/${id}/styles`
+
+// export default function () {
+//   // http.get(url);
+//   // sleep(1);
+//   const responses = http.batch([
+//     ['GET', urlProduct],
+//     ['GET', urlStyles]
+//   ]);
+// }
